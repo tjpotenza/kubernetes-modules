ef552c2778d8e8c32d6a536aa4df39efa6dc2b6c
TJ Potenza <thomas@potenza.me>
2021-01-15 21:25:22 -0500

    Add target group as an output for the control_plane module.

--- control_plane/outputs.tf
+++ control_plane/outputs.tf
@@ -9,3 +9,7 @@ output "control_plane_internal_address" {
 output "cluster_member_security_group_id" {
   value = aws_security_group.cluster_member.id
 }
+
+output "target_group_arn" {
+  value = aws_lb_target_group.ingress.arn
+}
